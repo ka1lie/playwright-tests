b85690bae5a3fc578f76d52093022669d8b08484
ka1lie <baka109g@gmail.com>
2024-11-20 18:13:04 +0400

    Added screenshots datetime and counter

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from playwright.sync_api import sync_playwright
+from datetime import datetime
+
+datetime = datetime.now()
+now = datetime.strftime('%m-%d-%Y-%H-%M-%S')
+
+counter = 0
 
 def sync_work():
     # открыть соединение
@@ -10,18 +16,30 @@ def sync_work():
         # переход по url адресу:
         page.goto('https://google.com/')
         # нажать на кнопку
-        page.get_by_role("button", name="Принять все").click()
-        # сделать скриншот
-        page.screenshot(path='./demo.png')
+        #page.get_by_role("button", name="Принять все").click()
+        # декларируеми функцию создания скриншотов
+        def take_screenshot():
+            global counter
+            page.screenshot(path='./screenshots/test-' + str(now) + '-' + str(counter) + '.png')
+            counter = counter + 1
+        take_screenshot()
         # заполнть поле и выполнить поиск
         page.get_by_label("Найти").fill("makima cosplay"); page.get_by_label("Найти").press("Enter")
         # еще один скриншот
-        page.screenshot(path='./demo1.png')
+        take_screenshot()
+        # закрыть браузер
         browser.close()
 
-try:
-    sync_work()
-    if True:
-        print("All Works!")
-except (RuntimeError, TypeError, NameError):
-    pass
+
+# сделал отдельную функцию result потому что планирую ее отправлять на web-server чтобы проверять что у нас
+# все гуд по API
+
+class APIResult():
+    try:
+        sync_work()
+        if True:
+            print("All Works!")
+    except (RuntimeError, TypeError, NameError):
+        pass
+
+result = APIResult()
