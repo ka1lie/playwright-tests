89f5d1c7c2497b18173ba39259b0274edb4179d3
ka1lie <baka109g@gmail.com>
2024-11-27 01:30:22 +0400

    Trying to accept POST requests

--- web.py
+++ web.py
@@ -1,20 +1,30 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from main import result
+import os
+from dotenv import load_dotenv
 
-HOST = "127.0.0.1"
-PORT = 8080
+# импортируем переменные из dotenv
+load_dotenv()
 
 class NeuralHTTP(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            content = open('index.html', 'rb').read()
+            self.wfile.write(content)
+        else:
+            self.send_response(404)
+    def do_POST(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+        else:
+            self.send_response(501)
 
-        self.wfile.write(bytes("HELLO", "utf-8"))
-
-
-server = HTTPServer((HOST, PORT), NeuralHTTP)
+server = HTTPServer((os.getenv('HOST'), int(os.getenv('PORT'))), NeuralHTTP)
 print("Server is running")
 server.serve_forever()
 server.server_close()
