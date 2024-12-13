308810d037a2ee243871c8ce204995f985a056e2
ka1lie <baka109g@gmail.com>
2024-12-13 11:07:51 +0400

    Rework web server to accept POST requests

--- web.py
+++ web.py
@@ -1,30 +1,53 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import os
+from http import HTTPStatus
 from dotenv import load_dotenv
+import json
+import time
+import os
 
 # импортируем переменные из dotenv
 load_dotenv()
 
-class NeuralHTTP(BaseHTTPRequestHandler):
+checker_status = [{"status": "N/A"}]
+
+
+class _RequestHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(HTTPStatus.OK.value)
+        self.send_header('Content-type', 'application/json')
+        # Allow requests from any origin, so CORS policies don't
+        # prevent local development.
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
 
     def do_GET(self):
-        if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            content = open('index.html', 'rb').read()
-            self.wfile.write(content)
-        else:
-            self.send_response(404)
-    def do_POST(self):
-        if self.path == "/":
-            self.send_response(200)
-            self.send_header('Content-type','text/html')
-            self.end_headers()
-        else:
-            self.send_response(501)
+        self._set_headers()
+        self.wfile.write(json.dumps(checker_status).encode('utf-8'))
 
-server = HTTPServer((os.getenv('HOST'), int(os.getenv('PORT'))), NeuralHTTP)
-print("Server is running")
-server.serve_forever()
-server.server_close()
+    def do_POST(self):
+        global checker_status
+        checker_status = []
+        length = int(self.headers.get('content-length'))
+        message = json.loads(self.rfile.read(length))
+        checker_status.append(message)
+        self._set_headers()
+        self.wfile.write(json.dumps({'success': True}).encode('utf-8'))
+
+    def do_OPTIONS(self):
+        # Send allow-origin header for preflight POST XHRs.
+        self.send_response(HTTPStatus.NO_CONTENT.value)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST')
+        self.send_header('Access-Control-Allow-Headers', 'content-type')
+        self.end_headers()
+
+
+def run_server():
+    server_address = (os.getenv('HOST'), int(os.getenv('PORT')))
+    httpd = HTTPServer(server_address, _RequestHandler)
+    print('serving at %s:%d' % server_address)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run_server()
