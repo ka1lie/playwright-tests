10e2a28accb5543ead3c420ac4696a7590372e9a
ka1lie <baka109g@gmail.com>
2024-12-16 15:26:55 +0400

    Removed non-usable features

--- generate-key.py
+++ generate-key.py
@@ -8,17 +8,5 @@ print("Please wait for key generation")
 key = Fernet.generate_key()
 f = open("./creds/key", "wb")
 f.write(key)
-#read = open("./creds/key", "r").read().replace("b", "")
-
-#read.replace('b\'', '')
-#print(read())
 f.close
-
-
-f = open("./creds/key", "r")
-print(f.read())
-print(key)
-f.close
-#f.replace('b\'', '')
-
 
